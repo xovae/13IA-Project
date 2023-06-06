e345b85d323e33e9aad2c329fc3ab71af3ae1934
xovae <89520672+xovae@users.noreply.github.com>
2023-06-06 14:16:16 +1200

    Quiz 0.2
    Further development of Classes, moved to external file questionsTemplate.cs. Additional CSV files added for MultiSelect and TrueFalse questions, with different CSV headings. Unneeded resources in the parent file have been removed, as they are already in the Resources.resx file internally

--- 13IA Project/13IA Project/Questions.cs
+++ 13IA Project/13IA Project/Questions.cs
@@ -30,6 +30,8 @@ namespace _13IA_Project
         private void frmQuestions_Load(object sender, EventArgs e)
         {
             string[] current;
+            int distanceFromTop = 0;
+            int distanceFromLeft = 0;
             StreamReader sr = new StreamReader(filePath);
 
             while (!sr.EndOfStream)
@@ -37,7 +39,7 @@ namespace _13IA_Project
                 current = sr.ReadLine().Split(',');
                 if (current[0] == "Multichoice")
                 {
-                    multichoiceList.Add(new Multichoice(current[1], current[2], current[3], current[4], current[5], current[6]));
+                    multichoiceList.Add(new Multichoice(current[2], current[3], current[4], current[5], current[6]));
                 }
                 //else if (current[0] == "SelectAll")
                 //{
@@ -47,7 +49,8 @@ namespace _13IA_Project
             foreach (var item in multichoiceList)
             {
                 pnlQuestions.Controls.Add(item.panel);
-                //item.panel.Location = new Point(0, 0);
+                item.panel.Location = new Point(distanceFromLeft, distanceFromTop);
+                distanceFromTop += 500 - item.panel.Height;
             }
         }
 
@@ -68,82 +71,86 @@ namespace _13IA_Project
         public RadioButton option3 = new RadioButton();
         public RadioButton option4 = new RadioButton();
         
-        public Multichoice(string questionText, string option1Text, string option2Text, string option3Text, string option4Text, string answerText)
+        public Multichoice(string questionText, string correctAnswerText, string falseAnswer1, string falseAnswer2, string falseAnswer3)
         {
-            answer = answerText;
+            answer = correctAnswerText;
             panel.AutoSize = true;
             panel.BorderStyle = BorderStyle.Fixed3D;
+            option1.AutoSize = true;
+            option2.AutoSize = true;
+            option3.AutoSize = true;
+            option4.AutoSize = true;
 
             question.Text = questionText;
             panel.Controls.Add(question);
-            option1.Text = option1Text;
+            option1.Text = correctAnswerText;
             panel.Controls.Add(option1);
-            option2.Text = option2Text;
+            option2.Text = falseAnswer1;            //RANDOMLY SHUFFLE WHERE THE ANSWER IS 
             panel.Controls.Add(option2);
-            option3.Text = option3Text;
+            option3.Text = falseAnswer2;
             panel.Controls.Add(option3);
-            option4.Text = option4Text;
+            option4.Text = falseAnswer3;
             panel.Controls.Add(option4);
         }
     }
 
-    class MultiSelect : Multichoice
-    {
-        //public Panel panel = new Panel();
-        //public Label question = new Label();
-        //public string answer;
+    //class MultiSelect : Multichoice
+    //{
+    //    //public Panel panel = new Panel();
+    //    //public Label question = new Label();
+    //    //public string answer;
 
-        public CheckBox optionMulti1 = new CheckBox();
-        public CheckBox optionMulti2 = new CheckBox();
-        public CheckBox optionMulti3 = new CheckBox();
-        public CheckBox optionMulti4 = new CheckBox();
-        public CheckBox optionMulti5 = new CheckBox();
-        public CheckBox optionMulti6 = new CheckBox();
-        public CheckBox optionMulti7 = new CheckBox();
-        public CheckBox optionMulti8 = new CheckBox();
+    //    public CheckBox optionMulti1 = new CheckBox();
+    //    public CheckBox optionMulti2 = new CheckBox();
+    //    public CheckBox optionMulti3 = new CheckBox();
+    //    public CheckBox optionMulti4 = new CheckBox();
+    //    public CheckBox optionMulti5 = new CheckBox();
+    //    public CheckBox optionMulti6 = new CheckBox();
+    //    public CheckBox optionMulti7 = new CheckBox();
+    //    public CheckBox optionMulti8 = new CheckBox();
 
-        public MultiSelect(string questionText, string option1Text, string option2Text, string option3Text, string option4Text, string answerText)
-        {
-            question.Text = questionText;
-            panel.Controls.Add(question);
-            optionMulti1.Text = option1Text;
-            panel.Controls.Add(option1);
-            optionMulti2.Text = option2Text;
-            panel.Controls.Add(option2);
-            optionMulti3.Text = option3Text;
-            panel.Controls.Add(option3);
-            optionMulti4.Text = option4Text;
-            panel.Controls.Add(option4);
+    //    public MultiSelect(string questionText, string option1Text, string option2Text, string option3Text, string option4Text, string answerText)
+    //    {
+    //        question.Text = questionText;
+    //        panel.Controls.Add(question);
+    //        optionMulti1.Text = option1Text;
+    //        panel.Controls.Add(option1);
+    //        optionMulti2.Text = option2Text;
+    //        panel.Controls.Add(option2);
+    //        optionMulti3.Text = option3Text;
+    //        panel.Controls.Add(option3);
+    //        optionMulti4.Text = option4Text;
+    //        panel.Controls.Add(option4);
 
-            answer = answerText;
-            panel.AutoSize = true;
-        }
+    //        answer = answerText;
+    //        panel.AutoSize = true;
+    //    }
 
-        public MultiSelect(string questionText, string option1Text, string option2Text, string option3Text, string option4Text, string option5Text, string option6Text, string option7Text, string option8Text, string answerText)
-        {
-            question.Text = questionText;
-            panel.Controls.Add(question);
-            optionMulti1.Text = option1Text;
-            panel.Controls.Add(optionMulti1);
-            optionMulti2.Text = option2Text;
-            panel.Controls.Add(optionMulti2);
-            optionMulti3.Text = option3Text;
-            panel.Controls.Add(optionMulti3);
-            optionMulti4.Text = option4Text;
-            panel.Controls.Add(optionMulti4);
-            optionMulti5.Text = option5Text;
-            panel.Controls.Add(optionMulti5);
-            optionMulti6.Text = option6Text;
-            panel.Controls.Add(optionMulti6);
-            optionMulti7.Text = option7Text;
-            panel.Controls.Add(optionMulti7);
-            optionMulti8.Text = option8Text;
-            panel.Controls.Add(optionMulti8);
+    //    public MultiSelect(string questionText, string option1Text, string option2Text, string option3Text, string option4Text, string option5Text, string option6Text, string option7Text, string option8Text, string answerText)
+    //    {
+    //        question.Text = questionText;
+    //        panel.Controls.Add(question);
+    //        optionMulti1.Text = option1Text;
+    //        panel.Controls.Add(optionMulti1);
+    //        optionMulti2.Text = option2Text;
+    //        panel.Controls.Add(optionMulti2);
+    //        optionMulti3.Text = option3Text;
+    //        panel.Controls.Add(optionMulti3);
+    //        optionMulti4.Text = option4Text;
+    //        panel.Controls.Add(optionMulti4);
+    //        optionMulti5.Text = option5Text;
+    //        panel.Controls.Add(optionMulti5);
+    //        optionMulti6.Text = option6Text;
+    //        panel.Controls.Add(optionMulti6);
+    //        optionMulti7.Text = option7Text;
+    //        panel.Controls.Add(optionMulti7);
+    //        optionMulti8.Text = option8Text;
+    //        panel.Controls.Add(optionMulti8);
 
-            answer = answerText;
-            panel.AutoSize = true;
+    //        answer = answerText;
+    //        panel.AutoSize = true;
 
             
-        }
-    }
+    //    }
+    //}
 }
---
+++ 13IA Project/13IA Project/questionsTemplate.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _13IA_Project
+{
+    public class QuestionTemplate
+    {
+        public string questionText;
+        public string questionTopic;
+        public string answerText;
+        public Panel panel;
+
+        public QuestionTemplate(string questionText, string questionTopic, string answerText)
+        {
+            this.questionText = questionText;
+            this.questionTopic = questionTopic;
+            this.answerText = answerText;
+        }
+    }
+
+    public class TrueFalse : QuestionTemplate
+    {
+        public RadioButton radioButton1;
+        public RadioButton radioButton2;
+
+        public TrueFalse(string questionText, string questionTopic, string correctAnswer, string falseAnswer1) : base(questionText, questionTopic, correctAnswer)
+        {
+            radioButton1.Text = correctAnswer; 
+            radioButton2.Text = falseAnswer1;
+        }
+    }
+
+    public class MultiChoice : QuestionTemplate
+    {
+        public RadioButton radioButton1;
+        public RadioButton radioButton2;
+        public RadioButton radioButton3;
+        public RadioButton radioButton4;
+
+        public MultiChoice(string questionText, string questionTopic, string correctAnswer, string falseAnswer1, string falseAnswer2, string falseAnswer3) : base(questionText, questionTopic, correctAnswer)
+        {
+            radioButton1.Text = correctAnswer;
+            radioButton2.Text = falseAnswer1;
+            radioButton3.Text = falseAnswer2;
+            radioButton4.Text = falseAnswer3;
+        }
+    }
+
+    //public class MultiSelect: QuestionTemplate
+    //{
+    //    public CheckBox checkBox1;
+    //    public CheckBox checkBox2;
+    //    public CheckBox checkBox3;
+    //    public CheckBox checkBox4;
+    //    public CheckBox checkBox5;
+    //    public CheckBox checkBox6;
+    //    public CheckBox checkBox7;
+    //    public CheckBox checkBox8;
+
+    //    public MultiSelect(string questionText, string questionTopic, string answer1, string answer2, string answer3, string answer4) : base(questionText, questionTopic, answer1)
+    //    {
+    //        checkBox1.Text = correctAnswer;
+    //        checkBox2.Text = falseAnswer1;
+    //        checkBox3.Text = falseAnswer2;
+    //        checkBox4.Text = falseAnswer3;
+    //    }
+        
+    //    public MultiSelect(string questionText, string questionTopic, string correctAnswer, ) : base(questionText, questionTopic, correctAnswer)
+    //    {
+    //        checkBox1.Text = correctAnswer;
+    //        checkBox2.Text = falseAnswer1;
+    //        checkBox3.Text = falseAnswer2;
+    //        checkBox4.Text = falseAnswer3;
+    //    }
+    //}
+}
